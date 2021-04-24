c231096839dac21498f1b435c77ebccc8b35c852
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2021-04-24 10:25:48 -0400

    Se creo el modelo Friend y el DataContext

---
+++ Parcial2Friend/Parcial2Friend/Models/Friend.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2Friend.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Ingresar Nombre entre a 3 a 30 Caracteres", MinimumLength = 3)]
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDay { get; set; }
+    }
+}
